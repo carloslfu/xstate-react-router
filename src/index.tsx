494ba6f02fcce3909b7969534ca8a41e9fae22f2
Carlos Galarza <carloslfu@gmail.com>
2018-11-11 13:45:38 -0500

    feat: add MachineState context and remove semicolons

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,33 @@
-import * as React from 'react';
+import * as React from 'react'
 import * as PropTypes from 'prop-types'
-import { Machine, MachineConfig, MachineOptions } from 'xstate';
-import { interpret } from 'xstate/lib/interpreter';
-import { getNodes } from 'xstate/lib/graph';
-import { matchesState } from 'xstate/lib/utils';
-import * as toRegex from 'path-to-regexp';
-import { withRouter } from 'react-router';
+import { Machine, MachineConfig, MachineOptions, State } from 'xstate'
+import { interpret, Interpreter } from 'xstate/lib/interpreter'
+import { getNodes } from 'xstate/lib/graph'
+import { matchesState } from 'xstate/lib/utils'
+import * as toRegex from 'path-to-regexp'
+import { withRouter } from 'react-router'
 
 function matchURI(path, uri) {
-  const keys: any = [];
-  const pattern = toRegex(path, keys);
-  const match = pattern.exec(uri);
-  if (!match) return null;
-  const params = Object.create(null);
+  const keys: any = []
+  const pattern = toRegex(path, keys)
+  const match = pattern.exec(uri)
+  if (!match) return null
+  const params = Object.create(null)
   for (let i = 1; i < match.length; i++) {
-    params[keys[i - 1].name] = match[i] !== undefined ? match[i] : undefined;
+    params[keys[i - 1].name] = match[i] !== undefined ? match[i] : undefined
   }
-  return params;
+  return params
 }
 
-const routerEventPrefix = 'Router_';
+const routerEventPrefix = 'Router_'
 
-const RouterMachineContext = React.createContext(undefined);
+const RouterMachineContext = React.createContext<Interpreter<any, any, any> | undefined>(undefined)
 
-export const RouterMachine = RouterMachineContext.Consumer;
+const MachineStateContext = React.createContext<State<any, any> | undefined>(undefined)
+
+export const MachineState = MachineStateContext.Consumer
+
+export const RouterMachine = RouterMachineContext.Consumer
 
 export interface XStateRouterProps extends React.Props<any> {
   config: MachineConfig<any, any, any>
@@ -38,11 +42,12 @@ export interface PassedProps {
 export const XStateRouter = withRouter(
   class extends React.Component<XStateRouterProps & PassedProps, any> {
 
-    routes;
-    service;
-    debounceHistory;
-    debounceState;
-    unlistenHistory;
+    routes
+    service
+    machineState
+    debounceHistory
+    debounceState
+    unlistenHistory
 
     static propTypes = {
       config: PropTypes.object.isRequired,
@@ -50,80 +55,83 @@ export const XStateRouter = withRouter(
     }
 
     constructor(props) {
-      super(props);
-      const config = this.props.config;
-      const nodes = getNodes(Machine(config));
+      super(props)
+      const config = this.props.config
+      const nodes = getNodes(Machine(config))
       this.routes = []
       for (const node of nodes) {
         if (node.meta && node.meta.path) {
-          this.routes.push([node.path, node.meta.path]);
+          this.routes.push([node.path, node.meta.path])
         }
       }
       // add router events to config
       if (!config.on) {
-        config.on = {};
+        config.on = {}
       }
       for (const route of this.routes) {
         config.on[routerEventPrefix + route[0].join('_')] =
-          '#(machine).' + route[0].join('.');
+          '#(machine).' + route[0].join('.')
       }
       // setup service
-      this.service = interpret(Machine(config, this.props.options));
-      this.service.start();
-      this.service.onTransition(this.handleXStateTransition);
+      const machine = Machine(config, this.props.options)
+      this.machineState = machine.initialState
+      this.service = interpret(machine)
+      this.service.start()
+      this.service.onTransition(this.handleXStateTransition)
       // initial route
-      this.handleRouterTransition(this.props.location);
-      this.unlistenHistory = this.props.history.listen(this.historyListener);
+      this.handleRouterTransition(this.props.location)
+      this.unlistenHistory = this.props.history.listen(this.historyListener)
     }
 
     historyListener = location => {
       if (this.debounceHistory) {
-        this.debounceHistory = false;
-        return;
+        this.debounceHistory = false
+        return
       }
-      this.handleRouterTransition(location, true);
+      this.handleRouterTransition(location, true)
     }
 
     handleRouterTransition(location, debounceHistory?: boolean) {
-      let matchingRoute;
+      let matchingRoute
       for (const route of this.routes) {
-        const params = matchURI(route[1], location.pathname);
+        const params = matchURI(route[1], location.pathname)
         if (params) {
-          matchingRoute = route;
-          break;
+          matchingRoute = route
+          break
         }
       }
       if (matchingRoute) {
-        this.debounceState = true;
-        this.service.send(routerEventPrefix + matchingRoute[0].join('_'));
-        const state = this.service.state.value;
+        this.debounceState = true
+        this.service.send(routerEventPrefix + matchingRoute[0].join('_'))
+        const state = this.service.state.value
         if (!matchesState(state, matchingRoute[0].join('.'))) {
           const stateNode = this.service.machine.getStateNodeByPath(
             this.service.state.tree.paths[0]
-          );
+          )
           if (stateNode.meta && stateNode.meta.path) {
             if (debounceHistory) {
-              this.debounceHistory = true;
+              this.debounceHistory = true
             }
-            this.props.history.replace(stateNode.meta.path);
+            this.props.history.replace(stateNode.meta.path)
           }
         }
       }
     }
 
     handleXStateTransition = state => {
+      this.machineState = state
       if (this.debounceState) {
-        this.debounceState = false;
-        return;
+        this.debounceState = false
+        return
       }
       const stateNode = this.service.machine.getStateNodeByPath(
         state.tree.paths[0]
-      );
+      )
       if (stateNode.meta && stateNode.meta.path) {
-        this.debounceHistory = true;
-        this.props.history.push(stateNode.meta.path);
+        this.debounceHistory = true
+        this.props.history.push(stateNode.meta.path)
       }
-    };
+    }
 
     componentWillUnmount() {
       this.service.off(this.handleRouterTransition)
@@ -133,9 +141,11 @@ export const XStateRouter = withRouter(
     render() {
       return (
         <RouterMachineContext.Provider value={this.service}>
-          {this.props.children}
+          <MachineStateContext.Provider value={this.machineState}>
+            {this.props.children}
+          </MachineStateContext.Provider>
         </RouterMachineContext.Provider>
-      );
+      )
     }
   }
-);
+)
